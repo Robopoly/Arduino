3e0e924dabe3433d852876a761f93335d725a7c2
Karl Kangur <karl.kangur@gmail.com>
2012-07-21 13:51:53 +0200

    Added header.

--- libraries/Robopoly/robopoly.h
+++ libraries/Robopoly/robopoly.h
@@ -1,3 +1,16 @@
+/***************************************************************************************
+ *
+ * Title:       Robopoly library v2.0
+ * File:        robopoly.h
+ * Date:        21.07.2012
+ * Authors:     Christophe Winter, Thierry Barras, Karl Kangur
+ * 
+ * Description: This library is made for EPFL's robotics club members: Robopoly. It
+ *              contains some specific functions for used modules (motors) and an agenda
+ *              function to do pseudo-multitasking.
+ *
+ ***************************************************************************************/
+
 #ifndef _ROBOPOLY_H
 #define _ROBOPOLY_H
 
